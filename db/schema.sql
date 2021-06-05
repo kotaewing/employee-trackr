9887575971e93d4c58202f9d7eb139bd6cb72e99
kotaewing <kota.ewing@gmail.com>
2021-06-05 13:37:22 -0600

    added foreign keys to role and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,11 @@ CREATE TABLE role (
     id INT PRIMARY KEY,
     title  VARCHAR(30) NOT NULL,
     salary DECIMAL(8,2),
-    department_id INT
+    department_id INT,
+    CONSTRAINT fk_department 
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id) 
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -19,7 +23,15 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    CONSTRAINT fk_role 
+    FOREIGN KEY (role_id) 
+    REFERENCES role(id) 
+    ON DELETE SET NULL,
+    manager_id INT,
+    CONSTRAINT fk_manager 
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id) 
+    ON DELETE SET NULL
 );
 
 
